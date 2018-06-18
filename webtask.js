db877b21e12f6e194dc3e227c0bdb655bc59dcac
Tyler Wymer <twymer@gmail.com>
2018-06-17 20:31:47 -0500

    Cron job setup and message saving

--- webtask.js
+++ webtask.js
@@ -2,8 +2,14 @@
 * @param context {WebtaskContext}
 */
 
+const { WebClient } = require('@slack/client');
+const moment = require('moment');
+
+// TODO avoid having to pass client/context constantly?
+
 const sendMessage = (slackClient, messageData) => {
-  slackClient.chat.postMessage(messageData)
+  // TODO Update these success/error conditions
+  slackClient.chat.postMessage(messageData.slackMessage)
     .then((res) => {
       // `res` contains information about the posted message
       console.log('Message sent: ', res.ts);
@@ -11,38 +17,94 @@ const sendMessage = (slackClient, messageData) => {
     .catch(console.error);
 };
 
+const parseMessage = (message) => {
+  // Messages come in the format of how long to delay and then the message
+  // Examples:
+  // 30 minutes Hey, have you finished your TPS reports yet?
+  // 5 hours No I was definitely not working at 4 am, again.
+  const messageRegex = /^(\d+) (hours|minutes) (.*)/;
+  const splitMessage = messageRegex.exec(message);
+
+  // TODO Handle errors for bad message formatting
+  // TODO is this actually idiomatic for checking nulls?
+  if (splitMessage !== null) {
+     return {
+      sendAt: moment().utc().add(splitMessage[1], splitMessage[2]).toDate(),
+      text: splitMessage[3],
+    };
+  }
+};
 
 const extractMessageData = (slackClient, context) => {
-  console.log('#### 3');
-  console.log(context);
+  // TODO start with a hash and merge later
+  // instead of this mess
   const userId = context.body.user_id;
   const userName = context.body.user_name;
   const conversationId = context.body.channel_id;
+  const message = parseMessage(context.body.text);
 
   return slackClient.users.profile.get({user: userId})
     .then((res) => {
-      console.log('#### 2');
-      console.log(res.profile);
-      return {
-        channel: conversationId,
-        text: 'Vamos Mexico!',
-        username: res.profile.display_name,
-        icon_url: res.profile.image_48,
+      const messageData = {
+        slackMessage: {
+          channel: conversationId,
+          text: message.text,
+          username: res.profile.display_name,
+          icon_url: res.profile.image_48,
+        },
+        sendAt: message.sendAt,
       };
+
+      return messageData;
     });
 };
 
+const saveMessage = (slackClient, context, messageData) => {
+  context.storage.get(function (error, data) {
+    if (error) return cb(error);
+    data = data || { messages: [] };
+
+    data.messages.push(messageData);
+
+    context.storage.set(data, function (error) {
+      if (error) return cb(error);
+    });
+  });
+};
+
+const sendMessages = (slackClient, context) => {
+  // TODO Is there a good way to not have to duplicate this?
+  context.storage.get(function (error, data) {
+    if (error) return cb(error);
+    data = data || { messages: [] };
+
+    // TODO Do this async but I forget the function name
+    // TODO Don't just bulk send everything
+    data.messages.forEach(function (message) {
+      sendMessage(slackClient, message);
+    });
+    data = { messages: [] };
+
+    context.storage.set(data, function (error) {
+      if (error) return cb(error);
+    });
+  });
+};
 
 module.exports = function(context, cb) {
-  const { WebClient } = require('@slack/client');
   const token = context.secrets.SLACK_TOKEN;
   const slackClient = new WebClient(token);
 
-  extractMessageData(slackClient, context)
-    .then((res) => {
-      console.log('#### 1');
-      sendMessage(slackClient, res);
-    });
+  // Determine if this is a slack command or a post request which
+  // we will (naively) assume is coming from the cron job ping.
+  if (context.body && context.body.command == '/delay') {
+    extractMessageData(slackClient, context)
+      .then((res) => {
+        saveMessage(slackClient, context, res);
+      });
+  } else {
+    sendMessages(slackClient, context);
+  }
 
   // TODO what should I actually return here?
   cb(null, 'Success!');
